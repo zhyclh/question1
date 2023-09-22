fbfb37bcdde1899b58182512b71f4921deccbf15
zhyclh <140569842+zhyclh@users.noreply.github.com>
2023-09-22 16:40:46 +0930

    Update question16_ii.sh

--- question16_ii.sh
+++ question16_ii.sh
@@ -2,5 +2,5 @@ cp -r dir16_1 dir16_1_copy
 rm -rf dir16_1_copy/dir16_5
 mv dir16_1/dir16_2/dir16_3/dir16_4 dir16_1/dir16_2/
 mv dir16_1/dir16_2/file16_2 dir16_1/dir16_2/file16_2_renamed
-rm -rf dir16_1_copy/dir16_5/file16_4
+
 
