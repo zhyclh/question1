347642b13aadf784532716370ecc8fba562663cb
zhyclh <140569842+zhyclh@users.noreply.github.com>
2023-09-22 16:59:27 +0930

    Update question16_i.sh

--- question16_i.sh
+++ question16_i.sh
@@ -1,10 +1,7 @@
-mkdir dir16_1
-mkdir dir16_1/dir16_2
-mkdir dir16_1/dir16_2/dir16_3
-mkdir dir16_1/dir16_2/dir16_3/dir16_4
+mkdir -p dir16_1/dir16_2/dir16_3/dir16_4
 touch dir16_1/dir16_2/dir16_3/dir16_4/file16_3
 touch dir16_1/dir16_2/file16_2
-mkdir dir16_1/dir16_5
+mkdir -p dir16_1/dir16_5
 touch dir16_1/dir16_5/file16_4
 touch dir16_1/dir16_5/file16_5
 touch dir16_1/file16_1
